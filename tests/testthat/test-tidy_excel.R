50164581056ce50f9bd064649366a0880b09c1da
Chuck <chuckleong21@gmail.com>
2019-07-29 01:20:07 +0800

    fixed excel file path

--- tests/testthat/test-tidy_excel.R
+++ tests/testthat/test-tidy_excel.R
@@ -4,8 +4,8 @@ library(doEDA)
 test_that("file is xls format or xlsx format", {
         csv <- "a.csv"
         expect_error(tidy_excel(csv))
-        xcel <- "~/Desktop/R/数据汇总.xlsx"
-        expect_equal(length(readxl::excel_sheets(xcel)), 1)
+        xcel <- readxl::readxl_example("datasets.xlsx")
+        expect_equal(length(readxl::excel_sheets(xcel)), 4)
         sheet_alias <- readxl::excel_sheets(xcel)
         out <- tidy_excel(xcel)
         expect_true(all(colnames(out) != sheet_alias))
