1dc7ed4553bf90dd753a4584064f8ad7218fa6c1
Chuck <chuckleong21@gmail.com>
2019-09-24 12:17:39 +0800

    added flatten param to tidy_excel

--- R/tidy_excel.R
+++ R/tidy_excel.R
@@ -2,6 +2,7 @@
 #'
 #' @param file \strong{.xls} or \strong{.xlsx} file
 #' @param ... arguments passed down to \link[readxl:read_excel]{read_excel()}
+#' @param flatten should output be displayed in tidy format for \code{TRUE}?
 #'
 #' @details This function wraps the steps for importing excel files having more
 #'   than 1 sheet through \code{mutate()} a \emph{sheetName} column after
@@ -12,24 +13,39 @@
 #' @return A \link[tibble]{tibble}
 #'
 #' @seealso \itemize{ \item \link[dplyr]{mutate} \item \link[purrr]{map} }
-#'
+#' @importFrom dplyr group_split
+#' @importFrom readxl excel_sheets read_excel
+#' @importFrom purrr map_df map
+#' @importFrom stats setNames
 #' @export
 #'
 #' @examples
-#' tidy_excel(readxl::readxl_example("type-me.xlsx"))
-tidy_excel <- function(file, ...) {
+#' library(readxl)
+#' tidy_excel(readxl_example("type-me.xlsx"))
+tidy_excel <- function(file, ..., flatten = FALSE) {
         file_suffix <- regmatches(file, regexpr("[^.]+$(.*)", file))
         if(!grepl("\\.xls?(.)$", file)) {
                 stop(paste("\nExpects xls or xlsx file but",
                            file_suffix, collapse = "", "instead."), call. = FALSE)}
-        sheet <- readxl::excel_sheets(file)
-        out <- readxl::read_excel(file, ...)
+        sheet <- excel_sheets(file)
+        out <- read_excel(file, ...)
         if(length(sheet) < 2) {
                 return(out)
         } else {
-                sheet %>%
-                        purrr::map_df(~{readxl::read_excel(file, sheet = .x) %>%
-                                        dplyr::mutate(sheetNames = .x)})
+                out <- sheet %>%
+                        map_df(~{read_excel(file, sheet = .x) %>%
+                                        mutate(sheetNames = .x)})
+                if(flatten) {
+                        if(any(is.na(out)))
+                                warning("Incoherent data cells are coerced into NAs",
+                                        call. = FALSE)
+                        return(out)
+                } else{
+                        seq_along(sheet) %>%
+                                map(~read_excel(path = file, sheet = .x)) %>%
+                                setNames(sheet)
+                }
+
         }
 
 }
--- tests/testthat/test-tidy_excel.R
+++ tests/testthat/test-tidy_excel.R
@@ -1,13 +1,29 @@
 context("import tidy excel data")
 library(doEDA)
+library(readxl)
+
+xcel <- readxl_example("datasets.xlsx")
+sheet_alias <- excel_sheets(xcel)
+out <- tidy_excel(xcel)
+
 
 test_that("file is xls format or xlsx format", {
         csv <- "a.csv"
         expect_error(tidy_excel(csv))
-        xcel <- readxl::readxl_example("datasets.xlsx")
-        expect_equal(length(readxl::excel_sheets(xcel)), 4)
-        sheet_alias <- readxl::excel_sheets(xcel)
-        out <- tidy_excel(xcel)
-        expect_true(all(colnames(out) != sheet_alias))
-        expect_true(is.data.frame(out))
+})
+
+test_that("output type is default to a list", {
+        expect_true(is.list(out))
+})
+
+test_that("output length and the number of sheet names are equal", {
+        expect_equal(length(sheet_alias), length(out))
+})
+
+test_that("a flattened output is a data.frame", {
+        expect_true(is.data.frame(suppressWarnings(tidy_excel(xcel, flatten = TRUE))))
+})
+
+test_that("Print warnings when it is necessary", {
+        expect_warning(tidy_excel(xcel, flatten = TRUE))
 })
